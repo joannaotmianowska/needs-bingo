703f7d62437f52d73a81024b47c9c65d6c9d4070
joannaotmianowska <otmianowska.joanna@gmail.com>
2024-05-09 13:50:05 +0200

    init layout with mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,107 @@
 import Link from "next/link";
 
+const needs = [
+  '🤗 akceptacja',
+  '🛡️ bezpieczeństwo',
+  '🤝 bliskość',
+  '🎉 celebracja życia',
+  '💕 czułość',
+  '☮️ harmonia',
+  '🧘‍♀️ integralność',
+  '❤️ miłość',
+  '👥 poczucie przynależności',
+  '👑 poczucie własnej wartości',
+  '🕊️ pokój',
+  '🙏 poważanie',
+  '💯 prawda',
+  '🧑‍🤝‍🧑 przyjaźń',
+  '🌱 rozwój osobisty',
+  '✨ samorealizacja',
+  '🔍 sens',
+  '😌 spokój',
+  '🙌 szacunek',
+  '💬 szczerość',
+  '🎨 twórczość',
+  '📚 uczenie się',
+  '🏆 uznanie',
+  '✈️ wolność',
+  '🙏 współczucie',
+  '🤝 współpraca',
+  '🤲 wsparcie',
+  '🔒 zaufanie',
+  '💚 zdrowie',
+  '😊 życzliwość'
+];
+
+const selfCareActivities = [
+  '🧘‍♀️ Medytacja i praktyki uważności',
+  '📖 Czytanie inspirujących książek lub poezji',
+  '🎨 Twórcze zajęcia, takie jak malowanie, rysowanie lub rzeźbienie',
+  '🌳 Spacery na świeżym powietrzu i kontakt z naturą',
+  '🛀 Relaksująca kąpiel z aromatycznymi olejkami',
+  '🍲 Przygotowywanie zdrowych i pożywnych posiłków',
+  '💆‍♀️ Masaż lub inne zabiegi pielęgnacyjne',
+  '🧘‍♂️ Praktyki oddechowe i relaksacyjne',
+  '🎵 Słuchanie ulubionej muzyki lub śpiewanie',
+  '🌱 Pielęgnowanie roślin lub ogródka',
+  '📝 Prowadzenie dziennika lub pisanie listów',
+  '🧶 Robótki ręczne, takie jak szydełkowanie lub hafciarstwo',
+  '🍵 Picie herbaty lub innego relaksującego napoju',
+  '🕯️ Tworzenie przytulnej atmosfery z pomocą świec i aromatów',
+  '🧖‍♀️ Wizyta w saunie lub łaźni parowej',
+  '🌞 Spędzanie czasu na świeżym powietrzu i opalanie się',
+  '🧘‍♂️ Praktyki jogi lub tai-chi',
+  '🎭 Udział w zajęciach teatralnych lub improwizacji',
+  '🎨 Malowanie lub rysowanie dla przyjemności',
+  '🏞️ Wycieczki na łono natury i obserwacja przyrody',
+  '🧘‍♀️ Praktyki uważności i medytacji',
+  '📚 Uczenie się nowych umiejętności lub hobby',
+  '🎶 Słuchanie relaksującej muzyki lub śpiewanie',
+  '💐 Otaczanie się pięknymi roślinami i kwiatami',
+  '🛀 Relaksujące kąpiele z solami lub olejkami',
+  '🧘‍♂️ Praktyki oddechowe i relaksacyjne',
+  '🍲 Przygotowywanie zdrowych i pożywnych posiłków',
+  '📖 Czytanie inspirujących książek lub poezji',
+  '🎨 Twórcze zajęcia, takie jak malowanie, rysowanie lub rzeźbienie'
+];
+
+const mockNeeds = [ needs[0], needs[1], needs[2], needs[12], needs[15] ];
+const mockActivities = selfCareActivities.slice(0, 9);
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
+    <main className="flex min-h-screen flex-col gap-6 items-center bg-gradient-to-b from-my-yellow to-my-beige text-white p-20 text-xl">
+      <h1 className="text-6xl pb-10">Zadbaj o swoje potrzeby 🧘‍♀️🌱☕️</h1>
+      <div className="bg-my-green rounded-2xl p-10 shadow-xl text-center">
+        <h2 className="text-5xl pb-5">Potrzeby, o które aktualnie chcesz zadbać to:</h2>
+        <ul className="text-3xl pb-5 flex flex-row flex-wrap gap-2">
+          {mockNeeds.map((need, index) => (
+            <li key={`need+${index}`}>{need}</li>
+          ))}
+        </ul>
+        <button className="px-6 text-2xl py-3 overflow-hidden rounded-2xl bg-my-yellow shadow-xl hover:shadow-md">
+          <span className="relative z-10 text-white">✏️ &nbsp; Edytuj aktualne potrzeby</span>
+        </button>
+      </div>
+      <div className="bg-my-green rounded-2xl p-10 shadow-xl text-center">
+        <h2 className="text-8xl pb-10">Potrzebowe bingo</h2>
+        <p className="text-xl font-text">Co dziś wybierzesz? Wykreśl element klikając na niego, gdy zrealizujesz zadanie.</p>
+        <div className="bg-white rounded-lg shadow-md p-6 mb-10 mt-10 text-3xl">
+          <div className="grid grid-cols-3 gap-4">
+            {mockActivities.map((activity, index) => (
+              <div
+                key={index}
+                className='rounded-lg p-8 flex items-center justify-center bg-rose-100 text-rose-600'
+              >
+                <span className="font-semibold">{activity}</span>
+              </div>
+            ))}
+          </div>
         </div>
+        <button className="px-6 text-2xl py-3 overflow-hidden rounded-2xl bg-my-yellow shadow-xl hover:shadow-md  mb-10">
+          <span className="relative z-10 text-white">✏️ &nbsp; Edytuj bingo</span>
+        </button>
+        <p className="text-xl font-text">Podziel się swoim bingo ze znajomymi!</p>
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,17 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        text: ['Lora', 'serif'],
+        textItalic: ['Lora-italic', 'serif'],
+        extra: ['Amatic', 'sans-serif'],
+        extraBold: ['Amatic-Bold', 'sans-serif'],
       },
+      colors: {
+        'my-green': '#A8B59F',
+        'my-beige': '#E8DBC6',
+        'my-rose': '#D2B2B8',
+        'my-yellow': '#D9C18D'
+      }
     },
   },
   plugins: [],
