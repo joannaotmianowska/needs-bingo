49afe6739735ff20711cc695e6e49903282b3e8b
joannaotmianowska <otmianowska.joanna@gmail.com>
2024-05-12 11:45:16 +0200

    drizzle kit version up to 0.21.0

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,13 @@
-import { type Config } from "drizzle-kit";
 
+import { defineConfig, type Config } from "drizzle-kit";
 import { env } from "~/env";
 
-export default {
-  schema: "./src/server/db/schema.ts",
-  driver: "pg",
-  dbCredentials: {
-    connectionString: env.POSTGRES_URL,
-  },
-  tablesFilter: ["needs-bingo_*"],
-} satisfies Config;
+
+export default defineConfig({
+    dialect: "postgresql",
+    schema: "./src/server/db/schema.ts",    
+    dbCredentials: {
+      url: env.POSTGRES_URL,
+    },
+    tablesFilter: ["needs-bingo_*"],
+}) satisfies Config;
